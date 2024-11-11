9cb2df7bbaf374d91ca38a297c22796399dcf7ed
hyoeunjeong <151709643+hyoeunjeong@users.noreply.github.com>
2024-11-11 22:40:57 +0100

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241112/정수 복사/copy-integer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a =3;
+    int b =4;
+    b= a;
+   cout << a ;
+   cout <<" " << b <<endl;
+   int c = a*b;
+   cout << c ; // 여기에 코드를 작성해주세요.
+    return 0;
+}
