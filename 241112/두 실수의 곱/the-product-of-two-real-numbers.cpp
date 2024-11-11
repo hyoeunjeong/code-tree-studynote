6134b6362ae6554bd91c6502e87f6d023e09ff56
hyoeunjeong <151709643+hyoeunjeong@users.noreply.github.com>
2024-11-11 22:15:30 +0100

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241112/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 5.26;
+    double b = 8.27;
+    
+    cout.fixed;
+    cout.precision(3);
+    
+    double c = a*b;
+    cout<<c<< endl;// 여기에 코드를 작성해주세요.
+    return 0;
+}
