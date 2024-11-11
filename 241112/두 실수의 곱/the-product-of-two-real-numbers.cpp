b511170a81eaa987c4683b4db42d967d227ed474
hyoeunjeong <151709643+hyoeunjeong@users.noreply.github.com>
2024-11-11 22:21:50 +0100

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241112/두 실수의 곱/the-product-of-two-real-numbers.cpp
+++ 241112/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <iomanip> // setprecision과 fixed를 사용하기 위해 필요
 using namespace std;
 
 int main() {
     double a = 5.26;
     double b = 8.27;
-    
-    cout.fixed;
-    cout.precision(3);
-    
-    double c = a*b;
-    cout<<c<< endl;// 여기에 코드를 작성해주세요.
+
+    double c = a * b;
+
+    cout << fixed << setprecision(3); // 소수점 셋째 자리까지 고정 소수점 표기법으로 출력
+    cout << c << endl; // 결과 출력
+
     return 0;
 }
