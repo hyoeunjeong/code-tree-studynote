41dde322664d3dbb91d9f8a08b76feeb473ddf76
hyoeunjeong <151709643+hyoeunjeong@users.noreply.github.com>
2024-11-12 11:47:40 +0100

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241112/데이터 교환/exchange-data.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int a = 5;
+ int b = 6;
+ int c =7;
+ int temp;
+ temp =a;
+ a=c;
+ c=b;
+ b = temp;
+
+ cout <<a <<endl;
+ cout <<b<<endl;
+ cout <<c<<endl;  // 여기에 코드를 작성해주세요.
+    return 0;
+}
